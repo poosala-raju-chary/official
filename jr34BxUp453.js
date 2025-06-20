527ec726c7c2f208c063f13e01aa7be0fde23e50
poosala-raju-chary <praju6900@gmail.com>
2025-06-20 10:17:26 +0500

    Update jr34BxUp453.js

--- jr34BxUp453.js
+++ jr34BxUp453.js
@@ -9,6 +9,7 @@
             //alert("test");
         });*/
 $(window).on('load', function () {
+      $('#content').hide('fast');      
       $('#loader').fadeOut(5000, function () {
         $('#content').fadeIn(7000);
       });

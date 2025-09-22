292974c8155e08cb6af6ef9d35fe568b135a01be
poosala-raju-chary <praju6900@gmail.com>
2025-09-22 10:23:00 +0530

    Update Ss8XjKQnRo7qNsP5weTCksmx2yJrCQyF.js

--- Ss8XjKQnRo7qNsP5weTCksmx2yJrCQyF.js
+++ Ss8XjKQnRo7qNsP5weTCksmx2yJrCQyF.js
@@ -1,1 +1,29 @@
+$(document).ready(function() {
+  let currentIndex = 0;
+  const slideWidth = 800;
+  const totalSlides = $('.slide').length;
+  let slideInterval;
 
+  function goToSlide(index) {
+    $('.slides-wrapper').css('transform', `translateX(-${index * slideWidth}px)`);
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % totalSlides;
+    goToSlide(currentIndex);
+  }
+
+  function startSlideshow() {
+    slideInterval = setInterval(nextSlide, 3000);
+  }
+
+  function stopSlideshow() {
+    clearInterval(slideInterval);
+  }
+
+  // Start slideshow on page load
+  startSlideshow();
+
+  // Pause on hover
+  $('.slideshow-container').mouseenter(stopSlideshow).mouseleave(startSlideshow);
+});
